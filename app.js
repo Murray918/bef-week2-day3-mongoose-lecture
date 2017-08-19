ece468faa99aec4457eefc80d29696f64221590a
Gil Pratte <gil@theironyard.com>
2017-08-19 11:40:09 -0500

    suffix for name

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const Recipe = require('./models/recipe')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/test');
 
-let name = 'Pancakes2'
+let suffix = 5;
+let name = 'Pancakes' + suffix
 const recipe = new Recipe({name: name, source: "Grandma"});
 recipe.save()
   .then(function () {
     console.log('saved ' + name);
-    return Recipe.findOne({name: "Pancakes"})
+    return Recipe.findOne({name: "Pancakes" + suffix})
   }).then(function(results) {
     console.log('findOne returned ' + results);
     return Recipe.find({cookTime: {$gt: 15, $lt: 60}})
