b4df431d36d17cab0d1c1218eb251ed68c09c85f
Gil Pratte <gil@theironyard.com>
2017-08-19 09:44:23 -0500

    findOne so far

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-CR of CRUD.
-
-Create, find one and find multiple.
-
-Is coded to use promise chaining (i.e. then then then).
-
-If a Recipe is created with same name (not a unique name) will cause an error.
+U of CRUD.
--- app.js
+++ app.js
@@ -4,17 +4,9 @@ const Recipe = require('./models/recipe')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/test');
 
-let name = 'Pancakes2'
-const recipe = new Recipe({name: name, source: "Grandma"});
-recipe.save()
-  .then(function () {
-    console.log('saved ' + name);
-    return Recipe.findOne({name: "Pancakes"})
-  }).then(function(results) {
+Recipe.findOne({name: "Pancakes"})
+  .then(function (results) {
     console.log('findOne returned ' + results);
-    return Recipe.find({cookTime: {$gt: 15, $lt: 60}})
-  }).then(function (results) {
-    console.log('find returned ' + results.length + ' results');
   }).catch(function (error) {
     console.log('error ' + JSON.stringify(error));
   })
