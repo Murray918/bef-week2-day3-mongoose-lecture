94d20cd7cebaf5fa0bc535f47aea96e9a255cf6f
Gil Pratte <gil@theironyard.com>
2017-08-23 07:15:42 -0500

    require Recipe

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // npm install mongoose@4.10.8 bluebird --save
 const mongoose = require('mongoose');
-const recipeModel = require('./models/recipe')
+const Recipe = require('./models/recipe')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/test');
 
-console.log(new recipeModel.Recipe({name: "Pancakes"}));
+console.log(new Recipe({name: "Pancakes5"}));
 
 process.on('SIGINT', function() {
   console.log("\nshutting down");
