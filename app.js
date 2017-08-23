a55bc227bfdc44a75411b71f9faa71bbbcee0632
Gil Pratte <gil@theironyard.com>
2017-08-23 07:27:14 -0500

    better logging

--- app.js
+++ app.js
@@ -4,20 +4,24 @@ const Recipe = require('./models/recipe')
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://localhost:27017/test');
 
-Recipe.findOne({name: "Pancakes"})
+let suffix = '6'
+Recipe.findOne({name: "Pancakes" + suffix})
   .then(function (results) {
-    console.log('findOne returned ' + results);
+    console.log('\nfindOne returned\n' + results);
     return Recipe.updateOne({source: "Grandma"},
     {$push: {steps: "Call Grandma and tell her how it was."}})
   }).then(function (results) {
-    console.log('updateOne returned ' + JSON.stringify(results));
+    console.log('\nupdateOne returned\n' + JSON.stringify(results));
+    return Recipe.findOne({name: "Pancakes" + suffix})
+  }).then(function (results) {
+    console.log('\nfindOne returned\n' + results);
     return Recipe.updateMany({source: "Grandma"},
     {$push: {steps: "Call Grandma and tell her how much the dog enjoyed it."}})
   }).then(function (results) {
-    console.log('updateMany returned ' + JSON.stringify(results));
+    console.log('\nupdateMany returned\n' + JSON.stringify(results));
     return Recipe.find({source: "Grandma"}).select("name");
   }).then(function (results) {
-    console.log('find returned ' + JSON.stringify(results));
+    console.log('\nfind source:Grandma returned\n' + JSON.stringify(results));
   }).catch(function (error) {
     console.log('error ' + JSON.stringify(error));
   })
